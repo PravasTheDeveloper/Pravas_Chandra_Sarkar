6b52eea0aea7efcfabbda610f2f9f599badc95f3
PravasTheDeveloper <info.pravas.cs@gmail.com>
2023-09-28 13:17:35 +0600

    testing homepage responsiveness

--- components/HeroHome/HeroSection.jsx
+++ components/HeroHome/HeroSection.jsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
         <>
             <div className='w-full flex-1 container mx-auto flex'>
                 <div className='md:w-1/2 w-full text-white'>
-                    <div className='md:w-[600px] w-full h-full  flex flex-col justify-center items-center md:text-left text-center'>
+                    <div className='md:w-[600px] w-full h-full  flex flex-col justify-center items-center md:text-left text-center mt-[-100px]'>
                         <div className='w-full h-auto md:text-4xl text-2xl'>
                             I'M <span className='text-yellow-color md:text-6xl text-6xl font-bold Hero_Name_Section'>Pravas</span>
                         </div>
@@ -30,7 +30,7 @@ export default function HeroSection() {
                                 Contact Me
                             </div>
                         </div>
-                        <div className='w-full h-10 text-white text-2xl mt-10 absolute bottom-10'>
+                        <div className='w-full h-10 text-white text-2xl mt-10 absolute bottom-28'>
                             <div className='md:w-[300px] w-full h-full flex items-center justify-between '>
                                 <div className='flex-1 h-[4px] rounded-full bg-yellow-color mr-10'>
 
