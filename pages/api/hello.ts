1039f537704b2a20b501099a3cb8c8a089947cf9
Pravas Chandra Sarkar <106769339+PravasTheDeveloper@users.noreply.github.com>
2024-01-14 23:39:55 +0600

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,7 +9,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if (req.method === "POST"){
+  if (req.method === "GET"){
     res.status(200).json({ messege: "hei" });
   }
 }
