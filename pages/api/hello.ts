cbbc136ec989ac9524b352db6ba4979944bbe127
Pravas Chandra Sarkar <106769339+PravasTheDeveloper@users.noreply.github.com>
2024-01-14 23:50:40 +0600

    Update hello.ts
    sfsa

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -11,11 +11,29 @@ export default async function handler(
 ) {
   if (req.method === "POST"){
     const { title, image } = req.body;
-    if (!title || !image) {
-      res.status(402).json({ messege: "Please Fill All The Feild" });
-    } else {
-       res.status(200).json({ messege: "Good Man" });
-    }
+
+            if (!title || !image) {
+                res.status(402).json({ messege: "Please Fill All The Feild" });
+            } else {
+                await connectToDb()
+
+                const findpreviousdata = await prisma.skilldata.findFirst({ where: { title: title } })
+
+                if (findpreviousdata) {
+                    res.status(401).json({ messege: "Feild Already Exist" });
+                    await prisma.$disconnect()
+                } else {
+                    const skilldatacreate = await prisma.skilldata.create({
+                        data: {
+                            title, image
+                        }
+                    })
+
+                    if (skilldatacreate) {
+                        res.status(200).json({ messege: "Created Successfully" });
+                    }
+                }
+            }
   }else{
    res.status(201).json({ messege: "You Are Bad" });
   }
